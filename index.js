395afc6bd14070c1303da305ad372a0338281867
NirajGadara <120002683+NirajGadara@users.noreply.github.com>
2023-03-08 23:37:07 +0530

    js code

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-const grids = document.getElementById("grids");    
+// function that builds a grid in the "container"
 
-function gridsize()
-{
-    let x = prompt ("Enter Number");
-    let y = prompt ("Enter Number");
 
- 
-    function makeRows(rows, cols)
-    {
-        grids.style.setProperty('--grid-rows', rows);
-        grids.style.setProperty('--grid-cols', cols);
-            for ( c = 0; c < (rows * cols); c++)
-            {
-                let cell = document.createElement("div");
-                grids.appendChild(cell).className = "grid-item";
-            };
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
     };
-makeRows(x, y);
-}
+    $(".grid").width(700/x);
+    $(".grid").height(700/x);
+};
+ 
+// function that clears the grid
+function clearGrid(){
+    $(".grid").remove();
+};  
+
+// function that prompts the user to select the number of boxes in a new grid
+// the function then also creates that new grid
+function refreshGrid(){
+    let x = prompt("Enter Grid Size");
+    clearGrid();
+    createGrid(x);
+};
+
+document.addEventListener("DOMContentLoaded", function(){
+    createGrid(16   );
+
+    let gridItem = document.querySelectorAll(".grid");
+    color = document.getElementById('color_piker')
+
+    gridItem.forEach(function(item){
+        color = document.getElementById('color_piker');
+        item.addEventListener("mouseover", function(){
+        this.style.backgroundColor = color.value;
+        })
+    })
+})
+// create a 16x16 grid when the page loads
+// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
+// allows the click of a button to prompt the user to create a new grid
+$(document).ready(function() {
+
+    $(".grid").mouseover(function() {
+        $(this).css("background-color", );
+        });
+
+    $("#Gridsize").click(function() {
+        refreshGrid();
+
+        $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
+    });
+});
